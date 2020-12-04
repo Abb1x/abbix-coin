066f99a445a0faefd9da142560139051c4c3dc94
Pherelo_HD <44616889+PhereloHD@users.noreply.github.com>
2020-12-04 22:36:51 +0100

    Update for main.cpp
    A loop that makes the Life easier.

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,33 @@
 #include "blockchain/blockchain.hpp"
 
-int main()
-{
-    Blockchain block_chain = Blockchain();
 
-    std::cout << "Mining block 1" << std::endl;
+
+
+
+
+int main(){
+
+int times;
+
+std::cout << "How many Blocks do you want to Mine?" << std::endl;
+std::cin >> times;
+
+
+for (int mine = 0; mine <= times; mine++){
+        Blockchain block_chain = Blockchain();
+
+    std::cout << "Mining block... " << std::endl;
     block_chain.add_block(Block(1, "Block 1 Data"));
 
-    std::cout << "Mining block 2" << std::endl;
+    std::cout << "Mining block... " << std::endl;
     block_chain.add_block(Block(2, "Block 2 Data"));
 
-    std::cout << "Mining block 3" << std::endl;
+    std::cout << "Mining block... " << std::endl;
     block_chain.add_block(Block(3, "Block 3 Data"));
-
-    return 0;
+    
 }
+
+
+
+}
+
