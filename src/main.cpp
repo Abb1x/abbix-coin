4b2e7c1411e7ba33dc9b1f4808a1bce21ff1341a
Ahmed <ahmed.hussain2080@gmail.com>
2020-12-07 03:07:20 +0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ int main()
     int i = 0;
     for(; 1 == 1 ;){
         i++;
-        std::cout << "Mining block "<<i<<std::endl;
+        std::cout << "Mining Abbix block "<<i<<std::endl;
         block_chain.add_block(Block(i, "Block 1 Data"));
     }
 
